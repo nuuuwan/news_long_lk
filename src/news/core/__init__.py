b7ce29e6dfc370c6f423393db71f318613049d6a
nuuuwan <nuuuwan@gmail.com>
2024-05-19 07:58:36 +0530

    moved article into folder

--- src/news/core/__init__.py
+++ src/news/core/__init__.py
@@ -1,4 +1,4 @@
 # news.core (auto generate by build_inits.py)
 
-from news.core.Article import Article
+from news.core.article import Article
 from news.core.ArticleHead import ArticleHead
---
+++ src/news/core/article/__init__.py
@@ -0,0 +1,3 @@
+# news.core.article (auto generate by build_inits.py)
+
+from news.core.article.Article import Article
